06004c2c8b6fa6f03cb12ff9ab163761d37832e4
oguzhansecgel <oguzhansecgel19@icloud.com>
2024-01-12 02:33:59 +0300

    Entityler oluşturuldu.

---
+++ Core/HepsiApi.Domain/Entities/Category.cs
@@ -0,0 +1,29 @@
+﻿using HepsiApi.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HepsiApi.Domain.Entities
+{
+    public class Category : EntityBase,IEntityBase
+    {
+        public Category()
+        {
+            
+        }
+        public Category(int parentID,string name, int priorty)
+        {
+            ParentID = parentID;
+            Name = name;
+            Priorty = priorty;
+        }
+
+        public required int ParentID { get; set; }
+        public required string Name { get; set; }
+        public required int Priorty { get; set; }
+        public ICollection<Detail> Details { get; set; }
+        public ICollection<Product> Products{ get; set; }
+    }
+}
