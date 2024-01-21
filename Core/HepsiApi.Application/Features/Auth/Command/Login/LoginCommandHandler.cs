320c4c8990e9c3e935790f86d51a2e835d7e5526
oguzhansecgel <oguzhansecgel19@icloud.com>
2024-01-21 21:18:31 +0300

    login controller yazıldı. token test edildi.

---
+++ Core/HepsiApi.Application/Features/Auth/Command/Login/LoginCommandHandler.cs
@@ -0,0 +1,68 @@
+﻿using HepsiApi.Application.Bases;
+using HepsiApi.Application.Features.Auth.Rules;
+using HepsiApi.Application.Interface.AutoMapper;
+using HepsiApi.Application.Interface.Tokens;
+using HepsiApi.Application.Interface.UnitOfWorks;
+using HepsiApi.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HepsiApi.Application.Features.Auth.Command.Login
+{
+    public class LoginCommandHandler : BaseHandler, IRequestHandler<LoginCommandRequest, LoginCommandResponse>
+    {
+        private readonly UserManager<User> userManager;
+        private readonly IConfiguration configuration;
+        private readonly ITokenService tokenService;
+        private readonly RoleManager<Role> roleManager;
+        private readonly AuthRules authRules;
+
+        public LoginCommandHandler(UserManager<User> userManager , IConfiguration configuration,ITokenService tokenService,RoleManager<Role> roleManager,AuthRules authRules ,IMapper mapper, IUnitOfWork unitOfWork, IHttpContextAccessor httpContextAccessor) : base(mapper, unitOfWork, httpContextAccessor)
+        {
+            this.userManager = userManager;
+            this.configuration = configuration;
+            this.tokenService = tokenService;
+            this.roleManager = roleManager;
+            this.authRules = authRules;
+        }
+
+        public async Task<LoginCommandResponse> Handle(LoginCommandRequest request, CancellationToken cancellationToken)
+        {
+            User user = await userManager.FindByEmailAsync(request.Email);
+            bool checkPassword = await userManager.CheckPasswordAsync(user, request.Password);
+
+            await authRules.EmailOrPasswordShouldNotBeInvalid(user, checkPassword);
+
+            IList<string> roles = await userManager.GetRolesAsync(user);
+
+            JwtSecurityToken token = await tokenService.CreateToken(user, roles);
+            string refreshToken = tokenService.GenerateRefreshToken();
+
+            _ = int.TryParse(configuration["JWT:RefreshTokenValidityInDays"], out int refreshTokenValidityInDays);
+
+            user.RefreshToken = refreshToken;
+            user.RefreshTokenExpiryTime = DateTime.Now.AddDays(refreshTokenValidityInDays);
+
+            await userManager.UpdateAsync(user);
+            await userManager.UpdateSecurityStampAsync(user);
+            var _token = new JwtSecurityTokenHandler().WriteToken(token);
+
+            await userManager.SetAuthenticationTokenAsync(user, "Default", "AccesToken", _token);
+
+            return new()
+            {
+                Token = _token,
+                RefreshToken = refreshToken,
+                Expiration = token.ValidTo
+            };
+        }
+    }
+}
---
+++ Core/HepsiApi.Application/Features/Auth/Exceptions/EmailOrPasswordShouldNotBeInvalidException.cs
@@ -0,0 +1,13 @@
+﻿using HepsiApi.Application.Bases;
+
+namespace HepsiApi.Application.Features.Auth.Exceptions
+{
+    public class EmailOrPasswordShouldNotBeInvalidException : BaseExceptions
+    {
+        public EmailOrPasswordShouldNotBeInvalidException() : base("Kullanıcı adı veya şifre yanlıştır. ") { }
+
+    }
+
+
+
+}
